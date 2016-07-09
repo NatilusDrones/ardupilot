1f337fafecee449702b37fa75fe87b6c843f21be
LZ <lz@snconsultants.com>
2016-07-09 17:58:02 -0400

    try again

--- ArduPlane/altitude.cpp
+++ ArduPlane/altitude.cpp
@@ -528,7 +528,7 @@ float Plane::rangefinder_correction(void)
 {
 #if RANGEFINDER_ENABLED == ENABLED
 /* changed 5000 to 6000 for testing */
-/* changed back to 5000 for testing */
+/* changed back to 5000 for testing @again */
     if (millis() - rangefinder_state.last_correction_time_ms > 5000) {
         // we haven't had any rangefinder data for 5s - don't use it
         return 0;
